323c830225cfed0894434bdadbb2fcc49829a46e
tarak-rowthu <taraknagavenkatadurgeshrowthu@gmail.com>
2026-02-19 10:13:32 +0530

    Ronaldo added UseCase2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,8 +1,19 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-                System.out.println("Welcome to Palindrome checker system");
-                System.out.println("Version: 1.0");
-                System.out.println("System initialised successfully");
+
+    public static void main(String[] args) {
+
+        // Hardcoded string (String Literal)
+        String word = "madam";
+
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
-
